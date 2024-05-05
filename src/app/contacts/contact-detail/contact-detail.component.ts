688100ca03c78988e7d7efc664d8997f21445724
goatcheese82 <nonsquishy@gmail.com>
2024-05-05 02:31:37 -0400

    init

---
+++ src/app/contacts/contact-detail/contact-detail.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cms-contact-detail',
+  templateUrl: './contact-detail.component.html',
+  styleUrl: './contact-detail.component.css'
+})
+export class ContactDetailComponent implements OnInit{
+  constructor() { }
+  ngOnInit(): void {
+
+  }
+}
